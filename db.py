663cc17655f0056a37313253de1ca8214b24e5a1
Ananyasdrafts <ananyag1019@gmail.com>
2024-09-14 23:12:07 +0530

    Set up database schema and connection

---
+++ db.py
@@ -0,0 +1,26 @@
+
+
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://ananya:mde6133#K@localhost/doc_retrieval"
+
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True, index=True)
+    frequency = Column(Integer, default=0)
+
+class Document(Base):
+    __tablename__ = 'documents'
+    doc_id = Column(Integer, primary_key=True, index=True)
+    document_text = Column(String)
+    document_embedding = Column(Float)
+
+Base.metadata.create_all(bind=engine)
